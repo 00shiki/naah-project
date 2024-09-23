1a1b078b4bcf814e4745aefaaf85bd32c93bd8fb
[MCL - Albertus Antaputra] <albertus.antaputra@haluancorp.com>
2024-09-23 21:16:27 +0700

    removed body in model

--- email-service/model/email.go
+++ email-service/model/email.go
@@ -3,7 +3,6 @@ package model
 type EmailPayload struct {
     To      		string `json:"to"`
     Subject 		string `json:"subject"`
-    Body    		string `json:"body"`
     Type    		string `json:"type"` // "verification" or "receipt"
 	VerificationURL string `json:"verification_url,omitempty"`
     OrderID       	string `json:"order_id,omitempty"`
